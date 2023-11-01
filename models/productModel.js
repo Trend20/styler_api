7571dc58c694ad4a19c801d6a6d464fac97b97de
Trend20 <enockomondi305@gmail.com>
2023-11-01 13:48:03 +0300

    adding product model

---
+++ models/productModel.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("product", productSchema);
+export default Product;
