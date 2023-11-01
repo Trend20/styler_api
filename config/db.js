99470f981bacb2a417054d56189afff9f04f3633
Trend20 <enockomondi305@gmail.com>
2023-11-01 11:53:09 +0300

    connecting to the database

--- config/db.js
+++ config/db.js
@@ -1,2 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const connectionUrl = process.env.MONGO_URL;
+
+const connectDB = () => {
+  mongoose
+    .connect(connectionUrl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+    .then(() => console.log("Database connected successfully!"))
+    .catch((err) => console.log(err.message || "Database connection failed"));
+};
+
+export default connectDB;
--- server.js
+++ server.js
@@ -2,11 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 // initialize an instance of express app
 const app = express();
 const PORT = process.env.PORT || 8090;
 dotenv.config();
+
+// database connection
+connectDB();
 
 // apply middleware
 app.use(cors());
