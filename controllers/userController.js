e35ebb1d3b150ba26ef5291e1dafc49df79e8a0c
Trend20 <enockomondi305@gmail.com>
2023-11-08 17:48:07 +0300

    adding authorization dependencies

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
+import User from "../models/userModel";
 
 const router = Router();
 
 // login user
+router.post("/login", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const newUser = await User.findOne({ name });
+    if (!newUser) {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {}
+});
 
 // register user
 
