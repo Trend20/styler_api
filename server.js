6d19c81fb956e771a1ad4eae07942f51d092766f
Trend20 <enockomondi305@gmail.com>
2023-11-08 16:59:11 +0300

    adding morgan for logging

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
-import bodyParser from "body-parser";
+import morgan from "morgan";
 import cors from "cors";
 import connectDB from "./config/db.js";
 
 // initialize an instance of express app
 const app = express();
-const PORT = process.env.PORT || 8090;
 dotenv.config();
 
 // define cors options
 const corsOptions = {
   origin: "http://localhost:8091",
 };
+const PORT = process.env.PORT || 8090;
 
 // database connection
 connectDB();
 
 // apply middleware
 app.use(cors(corsOptions));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({ message: "This is my styler backend!" });
